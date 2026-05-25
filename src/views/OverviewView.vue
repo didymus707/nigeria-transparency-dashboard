722678d0c5f16442fd99c2778bc89cc1ed30a02f
Adewale Orotayo <didymus7007@gmail.com>
2026-05-25 01:29:50 +0100

    feat(composables): add formatCurrency, formatNumber and formatPercent utilities

--- src/views/OverviewView.vue
+++ src/views/OverviewView.vue
@@ -1,10 +1,31 @@
 <template>
-  <KpiCard title="Total Contracts" value="1,432" info="↑ 12% from last year" />
-  <KpiCard title="Total Spend" value="₦284.5bn" info="↑ 8% from last year" />
-  <KpiCard title="Flagged Contracts" value="47"  />
+  <KpiCard
+    title="Total Contracts"
+    :value="computedContracts"
+    :info="computedContractsChange + ' from last year'"
+  />
+  <KpiCard
+    title="Total Spend"
+    :value="computedCurrency"
+    :info="computedSpendChange + ' from last year'"
+  />
+  <KpiCard title="Flagged Contracts" :value="computedFlagged" />
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 import KpiCard from '@/components/ui/KpiCard.vue';
+import { formatCurrency, formatNumber, formatPercent } from '@/composables/useFormatter';
 
+const rawContracts = ref(1432);
+const rawSpend = ref(284500000000);
+const rawFlagged = ref(47);
+const spendChange = ref(8);
+const contractsChange = ref(12);
+
+const computedContracts = computed(() => formatNumber(rawContracts.value));
+const computedCurrency = computed(() => formatCurrency(rawSpend.value));
+const computedFlagged = computed(() => formatNumber(rawFlagged.value));
+const computedSpendChange = computed(() => formatPercent(spendChange.value));
+const computedContractsChange = computed(() => formatPercent(contractsChange.value));
 </script>
